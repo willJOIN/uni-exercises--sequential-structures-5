8ddab0ec5450e43c26a4ba8c0b192f5aeadd9a73
willJOIN <whirano4@gmail.com>
2022-09-30 18:52:49 -0300

    fix: Corrige exercicio

--- Exercicio5.java
+++ Exercicio5.java
@@ -2,20 +2,17 @@ import java.util.Scanner;
 
 public class Exercicio5 {
     public static void main(String[] args) {
-        float raioCirculo, areaCirculo = 0;
-        final float pi = 3.1416f;
+        float valorMetro, valorCentimetro = 0;
 
         try {
-            System.out.println("Digite o raio do circulo:\n");
+            System.out.println("Digite o valor em metros:\n");
             Scanner scanner1 = new Scanner(System.in);
-            raioCirculo = scanner1.nextFloat();
+            valorMetro = scanner1.nextFloat();
 
-            scanner1.close();
-
-            areaCirculo = (float) (pi * (Math.pow(raioCirculo, 2)));
+            valorCentimetro = valorMetro * 100;
         } catch (Exception ex) {
-            System.out.println("Erro ao pegar raioCirculo e/ou areaCirculo no Exercicio5:\n" + ex);
+            System.out.println("Erro ao pegar valorCentimetro e/ou valorMetro no Exercicio5:\n" + ex);
         }
-        System.out.println("A area do circulo eh:\n" + areaCirculo);
+        System.out.println("O valor em centimetros eh:\n" + valorCentimetro);
     }
 }
